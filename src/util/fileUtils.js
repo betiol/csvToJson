59fd9276793463638189807b4ae6ed60bbed2b98
Nikollas Betiol <nikollasbetiol@gmail.com>
2019-04-15 09:48:22 -0400

    [fix] file encoding

--- src/util/fileUtils.js
+++ src/util/fileUtils.js
@@ -3,19 +3,18 @@
 let fs = require('fs');
 
 class FileUtils {
+	readFile(fileInputName) {
+		return fs.readFileSync(fileInputName, { encoding: 'latin1' }).toString();
+	}
 
-    readFile(fileInputName) {
-        return fs.readFileSync(fileInputName).toString();
-    }
-
-    writeFile(json, fileOutputName) {
-        fs.writeFile(fileOutputName, json, function (err) {
-            if (err) {
-                throw err;
-            } else {
-                console.log('File saved: ' + fileOutputName);
-            }
-        });
-    }
+	writeFile(json, fileOutputName) {
+		fs.writeFile(fileOutputName, json, function(err) {
+			if (err) {
+				throw err;
+			} else {
+				console.log('File saved: ' + fileOutputName);
+			}
+		});
+	}
 }
 module.exports = new FileUtils();
